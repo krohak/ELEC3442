ca015a77e57eafe2ec8d5e94d21568b78922acf2
krohak <krohak@hku.hk>
2019-03-15 14:26:35 +0800

    led control

--- lab4/client3.py
+++ lab4/client3.py
@@ -9,8 +9,9 @@ sense.clear()
 pygame.init()
 pygame.display.set_mode((640, 480))
 
-SERVER = '<Server IP>'
-PORT = 12345
+SERVER = '172.16.0.206'
+PORT = 12346
+
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.connect((SERVER, PORT))
 
@@ -32,9 +33,24 @@ while True:
 			sense.set_pixel(x, y, 255, 255, 255)
 		elif event.type == KEYDOWN and event.key == K_UP:
 			# Add some lines here
+			server.send('MOVE {0} {1} 0 0 0 '.format(x,y).encode())
+			sense.set_pixel(x, y, 0, 0, 0)
+			y = (y - 1) % 8
+			server.send('MOVE {0} {1} 255 255 255 '.format(x,y).encode())
+			sense.set_pixel(x, y, 255, 255, 255)
 		elif event.type == KEYDOWN and event.key == K_LEFT:
 			# Add some lines here
+			server.send('MOVE {0} {1} 0 0 0 '.format(x,y).encode())
+			sense.set_pixel(x, y, 0, 0, 0)
+			x = (x - 1) % 8
+			server.send('MOVE {0} {1} 255 255 255 '.format(x,y).encode())
+			sense.set_pixel(x, y, 255, 255, 255)
 		elif event.type == KEYDOWN and event.key == K_RIGHT:
 			# Add some lines here
+			server.send('MOVE {0} {1} 0 0 0 '.format(x,y).encode())
+			sense.set_pixel(x, y, 0, 0, 0)
+			x = (x + 1) % 8
+			server.send('MOVE {0} {1} 255 255 255 '.format(x,y).encode())
+			sense.set_pixel(x, y, 255, 255, 255)
 
 server.close()
